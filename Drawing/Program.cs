eb8b9b0fc10de714456445d32e809357fb235369
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2021-05-29 00:18:13 +0200

    Test

---
+++ Drawing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1900,1000,"Test");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                
+            }
+        }
+    }
+}
