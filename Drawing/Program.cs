f0a2690339714119b129bdd56b6a7e0c768ef2b8
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2021-05-31 13:30:33 +0200

    Lol

--- Drawing/Program.cs
+++ Drawing/Program.cs
@@ -5,31 +5,57 @@ using Raylib_cs;
 
 class Program
 {
-    public static bool isDrawing = false;
+    public static List<Stroke> allStrokes = new List<Stroke>();
+    public static Vector2 tempVector = new Vector2();
+    public static bool secondPos = false;
     static void Main(string[] args)
     {
         Raylib.InitWindow(1900, 1000, "Test");
         while (!Raylib.WindowShouldClose())
         {
+            if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON))
+                tempVector = Raylib.GetMousePosition();
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+            {
+                if (secondPos == false)
+                    secondPos = true;
+                else if (secondPos == true)
+                {
+                    Vector2[] vectors = new Vector2[2];
+                    vectors[0] = tempVector;
+                    vectors[1] = Raylib.GetMousePosition();
+
+                    secondPos = false;
+
+                    allStrokes.Add(new Stroke(vectors, 5, Color.BLACK));
+
+                    tempVector = Raylib.GetMousePosition();
+                }
+            }
+            Console.WriteLine(allStrokes.Count);
+
             Raylib.ClearBackground(Color.WHITE);
 
-            if (isDrawing)
+            for (int i = 0; i < allStrokes.Count; i++)
             {
-
+                Raylib.DrawCircleV(allStrokes[i].points[0], allStrokes[i].strokeWidth / 2, allStrokes[i].color);
+                Raylib.DrawCircleV(allStrokes[i].points[1], allStrokes[i].strokeWidth / 2, allStrokes[i].color);
+                Raylib.DrawLineEx(allStrokes[i].points[0], allStrokes[i].points[1], allStrokes[i].strokeWidth, allStrokes[i].color);
             }
 
             Raylib.EndDrawing();
         }
     }
 }
-
-class Object
+struct Stroke
 {
-    public List<Object> allObjects = new List<Object>();
-    public List<Vector2> points = new List<Vector2>();
-    public Object(List<Vector2> points)
+    public Vector2[] points { get; set; }
+    public int strokeWidth { get; set; }
+    public Color color { get; set; }
+    public Stroke(Vector2[] points, int strokeWidth, Color color)
     {
         this.points = points;
-        allObjects.Add(this);
+        this.strokeWidth = strokeWidth;
+        this.color = color;
     }
 }
